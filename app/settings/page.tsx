96b3e1511f92176ed078aad0a43ac975d13a27bf
siva venkata naga pavan putti <73236071+pavanputti@users.noreply.github.com>
2025-11-16 08:40:36 -0500

    Fix syntax error: remove extra quote from import statements

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 import { getServerSession } from 'next-auth'
-import { authOptions } from "@/lib/auth"'
+import { authOptions } from "@/lib/auth"
 import SettingsClient from '@/components/SettingsClient'
 
 export default async function SettingsPage() {
